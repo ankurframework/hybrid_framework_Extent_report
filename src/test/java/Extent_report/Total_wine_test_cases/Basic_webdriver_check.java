84d079eef30fc6a3ad2206e6091cff03390526d0
ankurframework <ankur.shandilya43@gmail.com>
2019-07-28 12:34:59 +0530

    modified and added space

--- src/test/java/Extent_report/Total_wine_test_cases/Basic_webdriver_check.java
+++ src/test/java/Extent_report/Total_wine_test_cases/Basic_webdriver_check.java
@@ -3,8 +3,8 @@ package Extent_report.Total_wine_test_cases;
 public class Basic_webdriver_check {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+   
+		
 	}
 
 }
